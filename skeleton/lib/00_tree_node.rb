fb73a3f0fde437f4fb18a740fb45e7b5c404b9cd
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-08-24 14:48:12 -0700

    Implement tree BFS & DFS

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class PolyTreeNode
 
   attr_reader :parent, :children, :value
@@ -21,18 +23,53 @@ class PolyTreeNode
   end
   
   def parent=(node)
-  if node==nil
-    @parent = nil
-    return
-  end
-  if parent.nil?
+  
+    if node==nil
+      @parent = nil
+      return
+    end
+  
+    if parent.nil?
       @parent = node
       @parent.children.push(self)
-  else
+    else
       @parent.children.delete(self)
       @parent = node
       @parent.children.push(self)
     end
-  end
     
+  end
+  
+  def add_child(child_node)
+    child_node.parent = self
+  end
+  
+  def remove_child(child_node)
+    raise "#{child_node} is not a child" unless @children.include?(child_node)
+    child_node.parent = nil
+  end
+  
+  def inspect
+    self.value
+  end
+  
+  def dfs(target_value)
+    return self if self.value == target_value
+    
+    self.children.each do |child|
+      child_search = child.dfs(target_value)
+      return child_search unless child_search.nil?
+    end
+    nil
+  end
+  
+  def bfs(target_value)
+    queue = [self]
+  
+    until queue.empty?
+      node = queue.shift
+      return node if node.value == target_value
+      queue += node.children
+    end
+  end
 end
